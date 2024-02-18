60a3e4485513d1068b7be2c7d9f5eda547437867
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-18 19:05:51 +0700

    fix session routes

---
+++ src/main/kotlin/me/sahmad/planningpoker/routes/SessionRoutes.kt
@@ -0,0 +1,26 @@
+package me.sahmad.planningpoker.routes
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.request.receive
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
+import me.sahmad.planningpoker.model.CreateSessionRequest
+
+fun Application.sessionRoutes() {
+    routing {
+        createSessionRoute()
+     }
+}
+
+fun Route.createSessionRoute() {
+    route("/session") {
+        post {
+            val request: CreateSessionRequest = call.receive<CreateSessionRequest>()
+
+            return@post
+         }
+    }
+}
