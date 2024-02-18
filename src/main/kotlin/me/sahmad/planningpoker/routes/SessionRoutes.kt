3a2a1a3fcf8e7c41d21ca9cbfde3611511898003
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-18 19:23:22 +0700

    fixed route

--- src/main/kotlin/me/sahmad/planningpoker/routes/SessionRoutes.kt
+++ src/main/kotlin/me/sahmad/planningpoker/routes/SessionRoutes.kt
@@ -3,11 +3,16 @@ package me.sahmad.planningpoker.routes
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import java.util.UUID
 import me.sahmad.planningpoker.models.CreateSessionRequest
+import me.sahmad.planningpoker.models.CreateSessionResponse
+import me.sahmad.planningpoker.models.SessionStorage
+import me.sahmad.planningpoker.models.User
 
 fun Application.sessionRoutes() {
     routing {
@@ -19,7 +24,11 @@ fun Route.createSessionRoute() {
     route("/session") {
         post {
             val request: CreateSessionRequest = call.receive<CreateSessionRequest>()
-
+            val sessionId = UUID.randomUUID()
+            val userId = UUID.randomUUID()
+            val user = User(name = request.name, userId = userId)
+            SessionStorage.sessions[sessionId] = listOf(user)
+            call.respond(CreateSessionResponse(sessionId))
             return@post
          }
     }
