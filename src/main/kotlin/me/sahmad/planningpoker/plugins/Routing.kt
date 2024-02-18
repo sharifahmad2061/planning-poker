637d71fcff24292a26c9129a255c1a803aee8f1b
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-19 00:38:26 +0700

    add createUserRoute to configureRouting

--- src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
+++ src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
@@ -2,7 +2,9 @@ package me.sahmad.planningpoker.plugins
 
 import io.ktor.server.application.Application
 import me.sahmad.planningpoker.routes.createSessionRoutes
+import me.sahmad.planningpoker.routes.createUserRoutes
 
 fun Application.configureRouting() {
     createSessionRoutes()
+    createUserRoutes()
 }
