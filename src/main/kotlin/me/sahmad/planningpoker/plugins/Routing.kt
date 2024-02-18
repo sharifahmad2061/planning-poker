236b1a715dbe0fd984e90225fecd24da6a88343d
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-18 22:58:18 +0700

    update function name

--- src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
+++ src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
@@ -1,8 +1,8 @@
 package me.sahmad.planningpoker.plugins
 
 import io.ktor.server.application.Application
-import me.sahmad.planningpoker.routes.sessionRoutes
+import me.sahmad.planningpoker.routes.createSessionRoutes
 
 fun Application.configureRouting() {
-    sessionRoutes()
+    createSessionRoutes()
 }
--- src/main/kotlin/me/sahmad/planningpoker/routes/SessionRoutes.kt
+++ src/main/kotlin/me/sahmad/planningpoker/routes/SessionRoutes.kt
@@ -5,22 +5,23 @@ import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import java.util.UUID
 import me.sahmad.planningpoker.models.CreateSessionRequest
 import me.sahmad.planningpoker.models.CreateSessionResponse
 import me.sahmad.planningpoker.models.SessionStorage
 import me.sahmad.planningpoker.models.User
-import java.util.UUID
 
-fun Application.sessionRoutes() {
+fun Application.createSessionRoutes() {
     routing {
-        createSessionRoute()
+        sessionRoutes()
     }
 }
 
-fun Route.createSessionRoute() {
+fun Route.sessionRoutes() {
     route("/session") {
         post {
             val request: CreateSessionRequest = call.receive<CreateSessionRequest>()
@@ -30,6 +31,13 @@ fun Route.createSessionRoute() {
             SessionStorage.sessions[sessionId] = listOf(user)
             call.respond(CreateSessionResponse(sessionId))
             return@post
+        }
+        get ("/{sessionId}") {
+            val sessionId: Result<UUID> = runCatching {
+                UUID.fromString(call.parameters["sessionId"])
+            }
+            val userId: Result<UUID> = sessionId.mapCatching { _ -> UUID.randomUUID() }
+            val user = User(userId = userId.getOrThrow())
         }
     }
 }
