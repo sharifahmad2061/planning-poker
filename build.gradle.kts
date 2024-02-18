2b4e8d40e4decf3bac7348ac7d49284925206aa6
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-18 19:05:41 +0700

    fix libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.6.3")
 
-    implementation("io.ktor:ktor-server-core:2.3.8")
-    implementation("io.ktor:ktor-server-netty:2.3.8")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.8")
-    implementation("io.ktor:ktor-server-websockets:2.3.8")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.8")
+    implementation("io.ktor:ktor-server-core-jvm:2.3.8")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.8")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.8")
+    implementation("io.ktor:ktor-server-websockets-jvm:2.3.8")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.8")
 
     implementation("ch.qos.logback:logback-classic:1.5.0")
 
