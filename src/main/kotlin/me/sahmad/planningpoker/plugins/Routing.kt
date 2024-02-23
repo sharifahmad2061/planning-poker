9cc6a2cb563823252aa9136ec0b5afddc253ee98
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-24 01:06:44 +0700

    installed home page routes

--- src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
+++ src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
@@ -1,10 +1,12 @@
 package me.sahmad.planningpoker.plugins
 
 import io.ktor.server.application.Application
+import me.sahmad.planningpoker.routes.createHomePageRoutes
 import me.sahmad.planningpoker.routes.createSessionRoutes
 import me.sahmad.planningpoker.routes.createUserRoutes
 
 fun Application.configureRouting() {
+    createHomePageRoutes()
     createSessionRoutes()
     createUserRoutes()
 }
