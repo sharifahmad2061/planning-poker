1d33a99cd1e45d0b726b8168842d5887b9e5eb70
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-24 23:27:12 +0700

    complete homepage and session routes

--- website/src/main/kotlin/me/sahmad/planningpoker/website/routes/HomePageRoutes.kt
+++ website/src/main/kotlin/me/sahmad/planningpoker/website/routes/HomePageRoutes.kt
@@ -3,18 +3,26 @@ package me.sahmad.planningpoker.website.routes
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.html.respondHtml
+import io.ktor.server.request.receiveParameters
+import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import io.ktor.server.sessions.sessions
+import io.ktor.server.sessions.set
+import java.util.UUID
 import kotlinx.html.FormEncType
 import kotlinx.html.FormMethod
 import kotlinx.html.body
 import kotlinx.html.form
 import kotlinx.html.h3
+import kotlinx.html.hiddenInput
 import kotlinx.html.p
 import kotlinx.html.submitInput
 import kotlinx.html.textInput
+import me.sahmad.planningpoker.common.models.User
 
 fun Application.createHomePageRoutes() {
     routing {
@@ -25,14 +33,20 @@ fun Application.createHomePageRoutes() {
 fun Route.homePageRoutes() {
     route("/") {
         get {
+            val origin: String? = call.parameters["origin"]
             call.respondHtml {
                 body {
                     h3 {
                         +"Login"
                     }
-                    form(action = "localhost:8080/login", encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
+                    form(action = "/login", encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
                         p {
                             textInput { +"name" }
+                        }
+                        if (!origin.isNullOrBlank()) {
+                            p {
+                                hiddenInput (name = "origin") { value = origin }
+                            }
                         }
                         p {
                             submitInput { value = "Submit" }
@@ -40,6 +54,19 @@ fun Route.homePageRoutes() {
                     }
                 }
             }
+            return@get
+        }
+        post {
+            val name: String? = call.receiveParameters()["name"]
+            if (name.isNullOrBlank()) call.respondRedirect("/", permanent = false)
+            else {
+                val user: User = User(name = name, id = UUID.randomUUID())
+                call.sessions.set(user)
+            }
+            val origin: String? = call.receiveParameters()["origin"]
+            if (!origin.isNullOrBlank()) call.respondRedirect(origin, permanent = false)
+            call.respondRedirect("/session", permanent = false)
+            return@post
         }
     }
 }
--- website/src/main/kotlin/me/sahmad/planningpoker/website/routes/SesssionRoutes.kt
+++ website/src/main/kotlin/me/sahmad/planningpoker/website/routes/SesssionRoutes.kt
@@ -1,19 +1,28 @@
 package me.sahmad.planningpoker.website.routes
 
-import io.ktor.http.HttpStatusCodeimport io.ktor.server.application.Application
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.parametersOf
+import io.ktor.http.path
+import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.html.respondHtml
+import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
+import java.util.UUID
 import kotlinx.html.FormEncType
 import kotlinx.html.FormMethod
 import kotlinx.html.body
 import kotlinx.html.form
 import kotlinx.html.h1
+import kotlinx.html.hiddenInput
 import kotlinx.html.p
 import kotlinx.html.submitInput
+import me.sahmad.planningpoker.common.models.User
 
 fun Application.createSessionRoutes() {
     routing {
@@ -24,18 +33,33 @@ fun Application.createSessionRoutes() {
 fun Route.sessionRoutes() {
     route("/session") {
         get {
-            call.respondHtml (HttpStatusCode.OK) {
-                body {
-                    h1 {
-                        +"New Session"
-                    }
-                    form ("localhost:8080/session", encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
-                        p {
-                            submitInput { value = "Start" }
+            val user: User? = call.sessions.get<User>()
+            if (user == null) call.respondRedirect("/", permanent = false)
+            else {
+                call.respondHtml (HttpStatusCode.OK) {
+                    body {
+                        h1 {
+                            +"New Session"
+                        }
+                        form ("localhost:8080/session", encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
+                            hiddenInput (name = "user") { value = user.id.toString() }
+                            p {
+                                submitInput { value = "Start" }
+                             }
                          }
                      }
-                 }
-             }
+                }
+            }
+        }
+        get ("/{sessionId}") {
+            val sessionId: UUID = UUID.fromString(call.parameters["sessionId"])
+            val user: User? = call.sessions.get<User>()
+            if (user == null) call.respondRedirect(permanent = false) {
+                path("/login")
+                parametersOf("origin", "/session/${sessionId}")
+            } else {
+                call.respondRedirect("/poker", permanent = false)
+            }
         }
     }
 }
