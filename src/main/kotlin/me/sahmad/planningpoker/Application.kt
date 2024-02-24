5309f1d01e7fd64df9fb5bf9876940f5b9eae32a
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-24 16:47:07 +0700

    install session dependency and add the fun to the module

--- src/main/kotlin/me/sahmad/planningpoker/Application.kt
+++ src/main/kotlin/me/sahmad/planningpoker/Application.kt
@@ -4,11 +4,13 @@ import io.ktor.server.application.Application
 import me.sahmad.planningpoker.plugins.configureCallLogging
 import me.sahmad.planningpoker.plugins.configureRouting
 import me.sahmad.planningpoker.plugins.configureSerialization
+import me.sahmad.planningpoker.plugins.configureSessionAuthentication
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     configureSerialization()
     configureCallLogging()
+    configureSessionAuthentication()
     configureRouting()
 }
