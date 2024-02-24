80306bc1df27a1b2355ac3deb734a2253f279f8c
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-24 23:26:57 +0700

    add poker routes to routing plugin

--- website/src/main/kotlin/me/sahmad/planningpoker/website/plugins/Routing.kt
+++ website/src/main/kotlin/me/sahmad/planningpoker/website/plugins/Routing.kt
@@ -3,9 +3,11 @@ package me.sahmad.planningpoker.website.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.routing.get
 import me.sahmad.planningpoker.website.routes.createHomePageRoutes
+import me.sahmad.planningpoker.website.routes.createPokerRoutes
 import me.sahmad.planningpoker.website.routes.createSessionRoutes
 
 fun Application.configureRouting() {
     createHomePageRoutes()
     createSessionRoutes()
+    createPokerRoutes()
 }
