57575f32a40ba2a727ecfee35ca933dfbd9bc3e3
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-24 23:29:24 +0700

    move session authentication to website

--- website/src/main/kotlin/me/sahmad/planningpoker/website/Application.kt
+++ website/src/main/kotlin/me/sahmad/planningpoker/website/Application.kt
@@ -1,9 +1,12 @@
 package me.sahmad.planningpoker.website
 
 import io.ktor.server.application.Application
+import me.sahmad.planningpoker.website.plugins.configureRouting
+import me.sahmad.planningpoker.website.plugins.configureSessionAuthentication
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     configureRouting()
+    configureSessionAuthentication()
 }
--- website/src/main/kotlin/me/sahmad/planningpoker/website/plugins/SessionAuthentication.kt
+++ website/src/main/kotlin/me/sahmad/planningpoker/website/plugins/SessionAuthentication.kt
@@ -1,10 +1,10 @@
-package me.sahmad.planningpoker.plugins
+package me.sahmad.planningpoker.website.plugins
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.sessions.Sessions
 import io.ktor.server.sessions.cookie
-import me.sahmad.planningpoker.models.User
+import me.sahmad.planningpoker.common.models.User
 
 fun Application.configureSessionAuthentication() {
     install(Sessions) {
