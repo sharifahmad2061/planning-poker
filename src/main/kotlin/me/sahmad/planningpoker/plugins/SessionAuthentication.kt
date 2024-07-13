c7c1c30be244b249a8a1522d88215ce992f461b9
sharif ahmad <sharif.ahmad@agoda.com>
2024-07-13 23:57:08 +0700

    update session auth

--- src/main/kotlin/me/sahmad/planningpoker/plugins/SessionAuthentication.kt
+++ src/main/kotlin/me/sahmad/planningpoker/plugins/SessionAuthentication.kt
@@ -1,10 +1,10 @@
-package me.sahmad.planningpoker.website.plugins
+package me.sahmad.planningpoker.plugins
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.sessions.Sessions
 import io.ktor.server.sessions.cookie
-import me.sahmad.planningpoker.common.models.User
+import me.sahmad.planningpoker.models.User
 
 fun Application.configureSessionAuthentication() {
     install(Sessions) {
