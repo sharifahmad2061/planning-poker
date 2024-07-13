42956c928f3b5dd8698a9c80e0cc0dd620e7d833
sharif ahmad <sharif.ahmad@agoda.com>
2024-07-14 00:04:53 +0700

    update session storage

--- src/main/kotlin/me/sahmad/planningpoker/models/SessionStorage.kt
+++ src/main/kotlin/me/sahmad/planningpoker/models/SessionStorage.kt
@@ -3,5 +3,20 @@ package me.sahmad.planningpoker.models
 import java.util.UUID
 
 object SessionStorage {
-    val sessions: MutableMap<UUID, List<User>> = mutableMapOf()
+    val sessions: MutableMap<UUID, Session> = mutableMapOf()
 }
+
+data class Session (
+    val users: List<User> = listOf(),
+    val pokerRounds: List<PokerRound> = listOf()
+)
+
+data class PokerRound (
+    val roundName: String,
+    val votes: List<Vote> = listOf()
+)
+
+data class Vote (
+    val user: User,
+    val vote: Int
+)
