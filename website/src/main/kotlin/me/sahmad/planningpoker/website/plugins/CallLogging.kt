45f6ee239bc3daeac70de79b091705ad811ad9a9
sharif ahmad <sharif.ahmad@agoda.com>
2024-07-13 23:59:13 +0700

    delete website module

--- website/src/main/kotlin/me/sahmad/planningpoker/website/plugins/CallLogging.kt
+++
@@ -1,9 +0,0 @@
-package me.sahmad.planningpoker.website.plugins
-
-import io.ktor.server.application.Application
-import io.ktor.server.application.install
-import io.ktor.server.plugins.callloging.CallLogging
-
-fun Application.configureCallLogging() {
-    install(CallLogging)
-}
