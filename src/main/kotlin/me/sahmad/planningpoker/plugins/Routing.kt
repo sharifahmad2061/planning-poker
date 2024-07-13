aeeed8b23f34f0922a017e67405dee9972aec061
sharif ahmad <sharif.ahmad@agoda.com>
2024-07-14 00:21:02 +0700

    update routing plugin

--- src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
+++ src/main/kotlin/me/sahmad/planningpoker/plugins/Routing.kt
@@ -3,10 +3,8 @@ package me.sahmad.planningpoker.plugins
 import io.ktor.server.application.Application
 import me.sahmad.planningpoker.routes.createHomePageRoutes
 import me.sahmad.planningpoker.routes.createSessionRoutes
-import me.sahmad.planningpoker.routes.createUserRoutes
 
 fun Application.configureRouting() {
     createHomePageRoutes()
     createSessionRoutes()
-    createUserRoutes()
 }
