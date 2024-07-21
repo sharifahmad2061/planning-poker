3d6cc1b36e56624c3ff3f8cedc7a8267670a7eab
sharif ahmad <sharif.ahmad@agoda.com>
2024-07-21 13:26:47 +0700

    add ping period to web sockets

--- src/main/kotlin/me/sahmad/planningpoker/plugins/WebSockets.kt
+++ src/main/kotlin/me/sahmad/planningpoker/plugins/WebSockets.kt
@@ -3,7 +3,11 @@ package me.sahmad.planningpoker.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.websocket.WebSockets
+import io.ktor.server.websocket.pingPeriod
+import java.time.Duration
 
 fun Application.configureWebSockets() {
-    install(WebSockets)
+    install(WebSockets){
+        pingPeriod = Duration.ofSeconds(15)
+    }
 }
