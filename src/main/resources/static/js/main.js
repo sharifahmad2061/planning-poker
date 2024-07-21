89f3ec5894c9fcbb7908f3dd59aeea294b362d40
sharif ahmad <sharif.ahmad@agoda.com>
2024-07-21 13:22:00 +0700

    add initial js for message sending and receiving

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,16 @@
+let new_round_form = document.getElementById('new_round_form');
+let current_round_form = document.getElementById('current_round_form');
+let completed_round_list = document.getElementById('past-rounds');
+
+websocket_connection = new WebSocket('ws://localhost:8080/ws');
+
+websocket_connection.onmessage = function (event) {
+    let message = JSON.parse(event.data);
+    console.log(message);
+}
+
+new_round_form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let formData = new FormData(new_round_form);
+    websocket_connection.send(JSON.stringify(formData));
+}
