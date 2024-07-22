eacb1142509b2f68074110cb9d5c0a85941367c8
sharif ahmad <sharif.ahmad@agoda.com>
2024-07-22 10:22:06 +0700

    add content conversion to plugin

--- src/main/kotlin/me/sahmad/planningpoker/plugins/WebSockets.kt
+++ src/main/kotlin/me/sahmad/planningpoker/plugins/WebSockets.kt
@@ -1,13 +1,16 @@
 package me.sahmad.planningpoker.plugins
 
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
 import java.time.Duration
+import kotlinx.serialization.json.Json
 
 fun Application.configureWebSockets() {
     install(WebSockets){
         pingPeriod = Duration.ofSeconds(15)
+        contentConverter = KotlinxWebsocketSerializationConverter(Json)
     }
 }
